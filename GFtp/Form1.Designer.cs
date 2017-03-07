3ded9f7cfe35e07e12f95c357d895893318186a1
gurum77 <leejaeho@empal.com>
2017-03-07 12:18:20 +0900

    GFtp v1.1
    GFtp v1.1

---
+++ GFtp/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace GFtp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectDirectoyButton = new System.Windows.Forms.Button();
+            this.ftpAddressTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toFtpButton = new System.Windows.Forms.Button();
+            this.connectionButton = new System.Windows.Forms.Button();
+            this.fromFtpButton = new System.Windows.Forms.Button();
+            this.fileGridView = new System.Windows.Forms.DataGridView();
+            this.ftpFileGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.fileGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ftpFileGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectDirectoyButton
+            // 
+            this.selectDirectoyButton.AutoSize = true;
+            this.selectDirectoyButton.Image = ((System.Drawing.Image)(resources.GetObject("selectDirectoyButton.Image")));
+            this.selectDirectoyButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.selectDirectoyButton.Location = new System.Drawing.Point(13, 4);
+            this.selectDirectoyButton.Name = "selectDirectoyButton";
+            this.selectDirectoyButton.Size = new System.Drawing.Size(156, 70);
+            this.selectDirectoyButton.TabIndex = 1;
+            this.selectDirectoyButton.Text = "Directory";
+            this.selectDirectoyButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.selectDirectoyButton.UseVisualStyleBackColor = true;
+            this.selectDirectoyButton.Click += new System.EventHandler(this.selectDirectoyButton_Click);
+            // 
+            // ftpAddressTextBox
+            // 
+            this.ftpAddressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ftpAddressTextBox.Location = new System.Drawing.Point(563, 70);
+            this.ftpAddressTextBox.Name = "ftpAddressTextBox";
+            this.ftpAddressTextBox.Size = new System.Drawing.Size(303, 25);
+            this.ftpAddressTextBox.TabIndex = 3;
+            this.ftpAddressTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ftpAddressTextBox_KeyPress);
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.idTextBox.Location = new System.Drawing.Point(563, 8);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(421, 25);
+            this.idTextBox.TabIndex = 4;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTextBox.Location = new System.Drawing.Point(563, 39);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(421, 25);
+            this.passwordTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(471, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ftp address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(471, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(471, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Password";
+            // 
+            // toFtpButton
+            // 
+            this.toFtpButton.Image = ((System.Drawing.Image)(resources.GetObject("toFtpButton.Image")));
+            this.toFtpButton.Location = new System.Drawing.Point(374, 163);
+            this.toFtpButton.Name = "toFtpButton";
+            this.toFtpButton.Size = new System.Drawing.Size(94, 84);
+            this.toFtpButton.TabIndex = 10;
+            this.toFtpButton.UseVisualStyleBackColor = true;
+            this.toFtpButton.Click += new System.EventHandler(this.UploadToFtpButton_Click);
+            // 
+            // connectionButton
+            // 
+            this.connectionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connectionButton.Image = ((System.Drawing.Image)(resources.GetObject("connectionButton.Image")));
+            this.connectionButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.connectionButton.Location = new System.Drawing.Point(872, 69);
+            this.connectionButton.Name = "connectionButton";
+            this.connectionButton.Size = new System.Drawing.Size(112, 29);
+            this.connectionButton.TabIndex = 12;
+            this.connectionButton.Text = "Connect";
+            this.connectionButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.connectionButton.UseVisualStyleBackColor = true;
+            this.connectionButton.Click += new System.EventHandler(this.connectionButton_Click);
+            // 
+            // fromFtpButton
+            // 
+            this.fromFtpButton.Image = ((System.Drawing.Image)(resources.GetObject("fromFtpButton.Image")));
+            this.fromFtpButton.Location = new System.Drawing.Point(374, 253);
+            this.fromFtpButton.Name = "fromFtpButton";
+            this.fromFtpButton.Size = new System.Drawing.Size(94, 84);
+            this.fromFtpButton.TabIndex = 13;
+            this.fromFtpButton.UseVisualStyleBackColor = true;
+            this.fromFtpButton.Click += new System.EventHandler(this.fromFtpButton_Click);
+            // 
+            // fileGridView
+            // 
+            this.fileGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.fileGridView.ColumnHeadersHeight = 24;
+            this.fileGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.fileGridView.Location = new System.Drawing.Point(12, 80);
+            this.fileGridView.Name = "fileGridView";
+            this.fileGridView.RowTemplate.Height = 27;
+            this.fileGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.fileGridView.ShowEditingIcon = false;
+            this.fileGridView.Size = new System.Drawing.Size(356, 373);
+            this.fileGridView.TabIndex = 15;
+            // 
+            // ftpFileGridView
+            // 
+            this.ftpFileGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ftpFileGridView.ColumnHeadersHeight = 24;
+            this.ftpFileGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.ftpFileGridView.Location = new System.Drawing.Point(474, 108);
+            this.ftpFileGridView.Name = "ftpFileGridView";
+            this.ftpFileGridView.RowTemplate.Height = 27;
+            this.ftpFileGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.ftpFileGridView.ShowEditingIcon = false;
+            this.ftpFileGridView.Size = new System.Drawing.Size(510, 345);
+            this.ftpFileGridView.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 465);
+            this.Controls.Add(this.ftpFileGridView);
+            this.Controls.Add(this.fileGridView);
+            this.Controls.Add(this.fromFtpButton);
+            this.Controls.Add(this.connectionButton);
+            this.Controls.Add(this.toFtpButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.ftpAddressTextBox);
+            this.Controls.Add(this.selectDirectoyButton);
+            this.Font = new System.Drawing.Font("Dotum", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "Form1";
+            this.Text = "GFtp v1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ftpFileGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button selectDirectoyButton;
+        private System.Windows.Forms.TextBox ftpAddressTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button toFtpButton;
+        private System.Windows.Forms.Button connectionButton;
+        private System.Windows.Forms.Button fromFtpButton;
+        private System.Windows.Forms.DataGridView fileGridView;
+        private System.Windows.Forms.DataGridView ftpFileGridView;
+    }
+}
+
---
+++ GFtp/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace GFtp
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When the Form1 is loaded, display directory list to directoryTreeView control.
+        string _currentDirectory = @"c:\";
+        string _ftpAddress = @"ftp://ftp.novell.com";
+        string _id = "";
+        string _password = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DefaultFieldValue();
+            DisplayDefaultFieldValue();
+            RefreshFileListBoxOfCurrentDirectory();
+        }
+
+        // Set default field values.
+        void DefaultFieldValue()
+        {
+            _currentDirectory = Directory.GetCurrentDirectory();
+        }
+
+        // Display default field value to controls.
+        void DisplayDefaultFieldValue()
+        {
+            ftpAddressTextBox.Text = _ftpAddress;
+            idTextBox.Text = _id;
+            passwordTextBox.Text = _password;
+        }
+
+        // Refresh FileTreeView
+        // Display all file in the current directory.
+        void RefreshFileListBoxOfCurrentDirectory()
+        {
+            // Get all file list  directory list of my pc;
+            DirectoryInfo di = new DirectoryInfo(_currentDirectory);
+
+            // Get all sub directory
+            FileInfo[] fiArray = di.GetFiles("*.*");
+            GridFileInfo[] gridFileInfos = new GridFileInfo[fiArray.Length];
+            int idx = 0;
+            foreach(FileInfo fi in fiArray)
+            {
+                gridFileInfos[idx] = new GridFileInfo();
+                gridFileInfos[idx].Name = fi.Name;
+                gridFileInfos[idx].Size = fi.Length;
+                idx++;
+            }
+            // Display file to grid view
+            DisplayFilesToGridView(fileGridView, gridFileInfos);
+        }
+
+        // Display file to grid view
+        void DisplayFilesToGridView(DataGridView gridView, GridFileInfo[] fileinfos)
+        {
+            if (fileinfos.Length == 0)
+            {
+                gridView.Rows.Clear();
+                return;
+            }
+
+            // initialize file tree view control
+            gridView.RowCount = fileinfos.Length;
+            gridView.ColumnCount = 2;
+
+            // set column's width
+            gridView.Columns[0].Width = 200;
+            gridView.Columns[1].Width = 150;
+
+            // set header titles
+            gridView.Columns[0].HeaderText = "Name";
+            gridView.Columns[1].HeaderText = "Size";
+
+            // Change column's sortmode to NotSortable for DataGridViewSelectionMode.FullColumnSelect
+            for (int i = 0; i < gridView.ColumnCount; ++i)
+            {
+                gridView.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            gridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            // Set files to grid view
+            int rowIndex = 0;
+            foreach (GridFileInfo f in fileinfos)
+            {
+                gridView.Rows[rowIndex].Height = 24;
+                gridView.Rows[rowIndex].Cells[0].Value = f?.Name;
+                gridView.Rows[rowIndex].Cells[1].Value = f?.Size;
+
+                rowIndex++;
+            }
+        }
+        // When the select director button clicked, select current directory and then refresh file tree view
+        private void selectDirectoyButton_Click(object sender, EventArgs e)
+        {
+            // select directory
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.SelectedPath = _currentDirectory;
+            if (dlg.ShowDialog() == DialogResult.Cancel)
+                return;
+            _currentDirectory = dlg.SelectedPath;
+
+            RefreshFileListBoxOfCurrentDirectory();
+        }
+
+        private void ftpFileListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ftpAddressTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != Convert.ToChar(Keys.Enter))
+                return;
+
+            RefreshFtpFileListBoxWithCurrentInput();
+        }
+
+        // Refresh ftp file list box
+        void RefreshFtpFileGridView(string ftpAddr, string id, string password)
+        {
+            GridFileInfo[] gridFileInfos    = GetAllGridFileInfosFromFtp(ftpAddr, id, password);
+
+            // display file list into list view
+            DisplayFilesToGridView(ftpFileGridView, gridFileInfos);
+        }
+
+        // Get all files on ftp root directory
+        private GridFileInfo[] GetAllGridFileInfosFromFtp(string ftpAddr, string id, string password)
+        {
+            Uri ftpUri = new Uri(ftpAddr);
+            FtpWebRequest ftpReq = (FtpWebRequest)WebRequest.Create(ftpUri);
+            ftpReq.Credentials = new NetworkCredential(id, password);
+            ftpReq.Timeout = 30000; // 30 seconds timeout
+            ftpReq.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+            FtpWebResponse ftpRes = (FtpWebResponse)ftpReq.GetResponse();
+
+            StreamReader reader = new StreamReader(ftpRes.GetResponseStream(), System.Text.Encoding.Default);
+
+            string str = reader.ReadToEnd();
+            string[] fileInfos = str.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            GridFileInfo[] gridFileInfos = new GridFileInfo[fileInfos.Length];
+            int idx = 0;
+            foreach(string infoString in fileInfos)
+            {
+                string[] infoArray = infoString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                gridFileInfos[idx] = new GridFileInfo();
+                gridFileInfos[idx].Size = long.Parse(infoArray[4]);
+                gridFileInfos[idx].Name = infoArray[8];
+                idx++;
+            }
+
+            ftpRes.Close();
+            return gridFileInfos;
+        }
+
+        // Translate files from local to ftp
+        private void UploadToFtpButton_Click(object sender, EventArgs e)
+        {
+            // Get files of local
+            string[] files = GetSelectedFilesOfCurrentDirectory();
+
+            if (TranslateFiles(files, true))
+            {
+                RefreshFtpFileGridView(_ftpAddress, _id, _password);
+                MessageBox.Show("Completed.");
+            }
+            else
+            {
+                MessageBox.Show("Failded Upload.");
+            }
+        }
+
+        // Translate files from local to ftp or from ftp to local
+        private bool TranslateFiles(string[] files, bool IsUpload)
+        {
+            // connect ftp
+            WebClient wc = new WebClient { Proxy = null };
+            wc.BaseAddress = _ftpAddress;
+            if (_id != "" && _password != "")
+            {
+                wc.Credentials = new NetworkCredential(_id, _password);
+            }
+
+            try
+            {
+                foreach (string filename in files)
+                {
+                    string pathName = Path.Combine(_currentDirectory, filename);
+                    // upload
+                    if (IsUpload)
+                    {
+                        wc.UploadFile(pathName, filename);
+                    }
+                    // download
+                    else
+                    {
+                        wc.DownloadFile(filename, pathName);
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // Get file list selected of current directory
+        private string[] GetSelectedFilesOfCurrentDirectory()
+        {
+            return GetSelectedFileOfGridView(fileGridView);
+        }
+
+        // Get file names selected in the grid view
+        private string[] GetSelectedFileOfGridView(DataGridView gridView)
+        {
+            string[] files = new string[gridView.SelectedRows.Count];
+            int idx = 0;
+            foreach (DataGridViewRow row in gridView.SelectedRows)
+            {
+                files[idx++] = row.Cells[0].Value.ToString();
+            }
+
+            return files;
+        }
+
+        // Get selected files on ftp
+        private string[] GetSelectedFilenamesOfFtp()
+        {
+            return GetSelectedFileOfGridView(ftpFileGridView);
+        }
+
+        // Connect to ftp
+        private void connectionButton_Click(object sender, EventArgs e)
+        {
+            RefreshFtpFileListBoxWithCurrentInput();
+          
+        }
+
+        // Refresh ftp file list box with current input (ftp address, id, password)
+        private void RefreshFtpFileListBoxWithCurrentInput()
+        {
+            // ftp 주소
+            _ftpAddress = ftpAddressTextBox.Text;
+
+            // id, password
+            _id = idTextBox.Text;
+            _password = passwordTextBox.Text;
+
+            // Connet to the ftp and get all file list.
+            RefreshFtpFileGridView(_ftpAddress, _id, _password);
+        }
+
+        // Download selected files on ftp.
+        private void fromFtpButton_Click(object sender, EventArgs e)
+        {
+            // Get files of ftp
+            string[] files = GetSelectedFilenamesOfFtp();
+
+            if (TranslateFiles(files, false))
+            {
+                RefreshFileListBoxOfCurrentDirectory();
+                MessageBox.Show("Completed.");
+            }
+            else
+            {
+                MessageBox.Show("Failded Download.");
+            }
+        }
+    }
+
+    public class GridFileInfo
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
