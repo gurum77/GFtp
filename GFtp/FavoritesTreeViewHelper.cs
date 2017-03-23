4e3b9404e9e211fdbc8235cc428cc68fcb172d4c
Unknown <leejaeho@empal.com>
2017-03-23 09:23:53 +0900

    GFtp v1.4 ing
    Working about favorites

--- GFtp/FavoritesTreeViewHelper.cs
+++ GFtp/FavoritesTreeViewHelper.cs
@@ -18,26 +18,46 @@ namespace GFtp
             setting.IgnoreWhitespace = true;
             setting.ConformanceLevel = ConformanceLevel.Fragment;
 
-            using (XmlReader reader = XmlReader.Create("favorites.xml", setting))
+            try
             {
-                string group = "";
-                while(reader.Read())
+                List<FavoritesItem> items   = null;
+                using (XmlReader reader = XmlReader.Create("favorites.xml", setting))
                 {
-                    if(reader.NodeType == XmlNodeType.Element)
+                    while (reader.Read())
                     {
-                        if(reader.Name == "Group")
+                        if (reader.NodeType == XmlNodeType.Element)
                         {
-                            group = reader.GetAttribute("name");
-                            continue;
+                            if (reader.Name == "Group")
+                            {
+                                items = favorites.GetGroup(reader.GetAttribute("name"));
+                                continue;
+                            }
+
+                            if (reader.Name == "Item")
+                            {
+                                if(items == null)
+                                {
+                                    continue;
+                                }
+
+                                FavoritesItem item  = new FavoritesItem();
+                                
+                                item.Name   = reader.GetAttribute("name");
+                                item.Address = reader.GetAttribute("address");
+                                item.Port = reader.GetAttribute("port");
+                                item.ID = reader.GetAttribute("id");
+                                item.Password   = reader.GetAttribute("password");
+                                
+                                items.Add(item);
+                            }
                         }
 
-                        if(reader.Name == "Item")
-                        {
-                            
-                        }
                     }
-                   
                 }
+            }
+            catch
+            {
+                MessageBox.Show("Not found favorites.xml");
             }
             return true;
         }
@@ -45,7 +65,53 @@ namespace GFtp
         // Refresh favorites items
         static public void RefreshFavoritesItems(this TreeView treeView, Favorites favorites)
         {
-            
+            treeView.Nodes.Clear();
+            foreach (var group in favorites._group)
+            {
+                string groupName = group.Key;
+
+                TreeNode groupNode  = new TreeNode();
+                groupNode.Text = groupName;
+                treeView.Nodes.Add(groupNode);
+
+                foreach (var item in group.Value)
+                {
+                    TreeNode itemNode = new TreeNode();
+                    itemNode.Text = item.Name;
+                    itemNode.ToolTipText = item.Address;
+                    groupNode.Nodes.Add(itemNode);
+                }
+            }
+
+            treeView.ExpandAll();
+        }
+
+        // Get selected favorites item
+        static public FavoritesItem GetSelectedFavoritesItem(this TreeView treeView, Favorites favorites)
+        {
+            TreeNode selectedNode   = treeView.SelectedNode;
+            if (selectedNode == null)
+                return null;
+
+            TreeNode parentNode = selectedNode.Parent;
+            if (parentNode == null)
+                return null;
+
+            return favorites.FindItem(parentNode.Text, selectedNode.Text);
+        }
+
+        // Get selected favorites item group name
+        static public string GetSelectedFavoritesItemGroupName(this TreeView treeView, Favorites favorites)
+        {
+            TreeNode selectedNode = treeView.SelectedNode;
+            if (selectedNode == null)
+                return "";
+
+            TreeNode parentNode = selectedNode.Parent;
+            if (parentNode == null)
+                return "";
+
+            return parentNode.Text;
         }
     }
 
@@ -53,6 +119,44 @@ namespace GFtp
     // favorites item list
     public class Favorites
     {
-        public Dictionary<string, List<FavoritesItem>> _group;
+        public Dictionary<string, List<FavoritesItem>> _group = new Dictionary<string, List<FavoritesItem>>();
+        public FavoritesItem FindItem(string groupName, string itemName)
+        {
+            foreach(var item in _group[groupName])
+            {
+                if (item.Name == itemName)
+                    return item;
+            }
+
+            return null;
+        }
+
+        // get group if exists group otherwise create group and return it.
+        public List<FavoritesItem> GetGroup(string groupName)
+        {
+            List<FavoritesItem> items;
+            try
+            {
+                items = _group[groupName];
+            }
+            catch
+            {
+                items = new List<FavoritesItem>();
+                _group[groupName] = items;
+            }
+
+            return items;
+        }
+
+        public bool AddItem(string groupName, FavoritesItem item)
+        {
+            List<FavoritesItem> items = GetGroup(groupName);
+            if (items == null)
+                return false;
+
+            items.Add(item);
+
+            return true;
+        }
     }
 }
